0d6c2f51363b6cf69fe55aaca6614f8d1a0b9797
Alba Garcia Gonzalez <garciagonzalezalba@gmail.com>
2023-11-22 16:49:49 +0100

    images added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,15 @@
+import './footer.css'
+const Footer = () => {
+    <div>
+        <div>
+            <img src="" alt="" />
+            <img src="" alt="" />
+            <img src="" alt="" />
+        </div>
+        <div>
+            <p>Terminos legales</p>
+        </div>
+    </div>
+}
+
+export default Footer
