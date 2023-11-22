3eab1fe99b22aa70495b5bc226424c0531f9bfa2
John Doe <johndoe@example.com>
2023-11-22 16:33:51 +0100

    structure

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,18 @@
 import { useState } from 'react'
 import './App.css'
+import NavBar from './components/NavBar'
+import Footer from './components/Footer'
+import MainPicture from './components/MainPicture'
+import Categories from './components/Categories'
+
 function App() {
 
   return (
     <>
-      
+      <NavBar/>
+      <Footer/>
+      <MainPicture/>
+      <Categories/>
     </>
   )
 }
