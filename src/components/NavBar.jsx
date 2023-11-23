cc303c37febf138a7a4d74783f08feb2a9475994
John Doe <johndoe@example.com>
2023-11-23 09:23:42 +0100

    NavBar updated

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -4,10 +4,10 @@ function NavBar() {
     return (
         <nav id="nav-bar">
             <img src="src/assets/navbar/LogoInstinto.MOBIL.png" alt="logo tienda" />
-            <div>
-                <img srl="" alt="logo" />
-                <img srl="" alt="logo" />
-                <img srl="" alt="logo" />
+            <div className='icons'>
+                <img src="src/assets/navbar/Search.png" className='icon' alt="logo search" />
+                <img src="src/assets/navbar/Profile.png" className='icon' alt="logo profile" />
+                <img src="src/assets/navbar/ShoppingCart.png" className='icon-cart' alt="logo cart" />
             </div>
         </nav>
     )
