584d76004059865e417d07ce8547d8e4ee2eeede
John Doe <johndoe@example.com>
2023-11-23 15:14:13 +0100

    merging productPage

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react'
 import './App.css'
 import NavBar from './components/NavBar'
 import Footer from './components/Footer'
-import Homepage from './components/Homepage'
+ import Homepage from './components/Homepage'
 {/*import ProductPage from '.components/ProductPage'*/ }
 {/*import MainPicture from './components/MainPicture'
 import Categories from './components/Categories'*/}
