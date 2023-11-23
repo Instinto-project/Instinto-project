170943d78a47bf24e4230d9756025dc92ae6c546
John Doe <johndoe@example.com>
2023-11-23 17:33:23 +0100

    ProductPage done

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -6,25 +6,37 @@ const ProductPage = () => {
             <div className='product-container'>
                 <img className="product-pic" src="src/assets/productPage/product1.png" alt="product image" />
                 <div className='info-container'>
-                    <h1>Pichi Rapunzel</h1>
-                    <p>Una prenda gruesa perfecta para ahora que entramos a una temperatúra más fresca. Diseñado y confeccionado en Barcelona.</p>
+                    <h1 className='product-name'>Pichi Rapunzel</h1>
+                    <p className='product-description'>Una prenda gruesa perfecta para ahora que entramos a una temperatúra más fresca. Diseñado y confeccionado en Barcelona.</p>
+                    <br/>
                     <div className='details-container'>
-                        <h3>Talla</h3>
-                        <div className='balls-container'> 
-                        <div className='ball-generic'></div>
-                        <div className='ball-generic'></div>
-                        <div className='ball-generic'></div>
-                        <div className='ball-generic'></div>
+                        <h3 className='det-name'>Talla</h3>
+                        <div className='balls-container'>
+                            <div className='ball-generic'></div>
+                            <div className='ball-generic'></div>
+                            <div className='ball-generic'></div>
+                            <div className='ball-generic'></div>
                         </div>
                     </div>
                     <div className='details-container'>
-                        <h3>Color</h3>
-                        <div className='balls-container'> 
-                        <div className='ball-green'></div>
-                        <div className='ball-purple'></div>
+                        <h3 className='det-name'>Color</h3>
+                        <div className='balls-container'>
+                            <div className='ball-green'></div>
+                            <div className='ball-purple'></div>
                         </div>
                     </div>
-                    <button></button>
+                    <br/>
+                    <button className='btn-add'>Añadir al carrito </button>
+                    <br/>
+                    <button className='btn-keep'>Seguir comprando</button>
+                    <br/>
+                    <h2 className='product-text-suggestion'>Completa el look</h2>
+                    <div className='product-pic-suggestion-container'>
+                        <img className="product-pic-suggestion" src="src/assets/productPage/product2.png" alt="product image2" />
+                        <img className="product-pic-suggestion" src="src/assets/productPage/product3.png" alt="product image3" />
+                        <img className="product-pic-suggestion" src="src/assets/productPage/product4.png" alt="product image4" />
+                        <img className="product-pic-suggestion" src="src/assets/productPage/product5.png" alt="product image5" />
+                    </div>
                 </div>
             </div>
         </div>
